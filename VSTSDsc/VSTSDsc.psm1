881e7473bd09c774556492f11e39618c264d0165
Wayne Hoggett <waynehoggett@hotmail.com>
2018-05-20 09:26:30 +1000

    Updated Test and Get Methods. Updated Readme.md

--- VSTSDsc/VSTSDsc.psm1
+++ VSTSDsc/VSTSDsc.psm1
@@ -1,25 +1,49 @@
 [DscResource()]
 class VSTSAgent {
     [DscProperty(Key)]
+    [string]$Pool
+
+    [DscProperty(Key)]
     [string]$Path
 
-    [DscProperty(Key)]
+    [DscProperty(Mandatory)]
     [string]$PAT
 
-    [DscProperty(Key)]
+    [DscProperty(Mandatory)]
     [string]$Uri
+
+    [DscProperty(NotConfigurable)]
+    [string]$ServiceName
 
     [void]Set(){
 
     }
 
     [bool]Test() {
-        return $true
-        #TODO: Update method
+        try {
+            Write-Verbose "Finding process names `"AgentService`" and `"Agent.Listener`""
+            if ((Get-Process -Name "Agent.Listener","AgentService").Count -eq 2) {
+                return $true 
+            } else {
+                return $false
+            }
+        } catch {
+            throw "Could not find process names `"AgentService`" and `"Agent.Listener`""
+        }
+
     }
 
     [VSTSAgent]Get() {
+
+        try {
+            Write-Verbose "Attempting to find the VSTS Service by searching for a service with a name like 'VSTSAgent'"
+            $this.ServiceName = "$((Get-Service | Where-Object {$_.Name -Like "*VSTSAgent*"}).ServiceName)"
+        } catch {
+            throw "Could not find VSTS Service by searching for a service with a name like 'VSTSAgent'"
+        }
+
         return @{
+            Pool = $This.Pool
             Path = $This.Path
             Uri = $This.Uri
         }
