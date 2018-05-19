17075a3dd5383475599ecd9c243a983db9155f2d
Wayne Hoggett <waynehoggett@hotmail.com>
2018-05-19 22:43:10 +1000

    Added initial file structure

---
+++ VSTSDsc/VSTSDsc.psm1
@@ -0,0 +1,18 @@
+[DscResource()]
+class VSTSAgent {
+    [DscProperty(Key)]
+    [string]$Property
+    #TODO: Add Properties
+
+    [void]Set(){
+
+    }
+
+    [bool]Test() {
+
+    }
+
+    [VSTSAgent]Get() {
+        
+    }
+}
