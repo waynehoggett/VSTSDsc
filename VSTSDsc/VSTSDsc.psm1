f3a3ef2de2fc978cea6e5b1bd7905e1cab986e21
Wayne Hoggett <waynehoggett@hotmail.com>
2018-05-19 22:47:21 +1000

    Added properties and updated get method

--- VSTSDsc/VSTSDsc.psm1
+++ VSTSDsc/VSTSDsc.psm1
@@ -1,18 +1,28 @@
 [DscResource()]
 class VSTSAgent {
     [DscProperty(Key)]
-    [string]$Property
-    #TODO: Add Properties
+    [string]$Path
+
+    [DscProperty(Key)]
+    [string]$PAT
+
+    [DscProperty(Key)]
+    [string]$Uri
 
     [void]Set(){
 
     }
 
     [bool]Test() {
-
+        return $true
+        #TODO: Update method
     }
 
     [VSTSAgent]Get() {
-        
+        return @{
+            Path = $This.Path
+            Uri = $This.Uri
+        }
+
     }
 }
